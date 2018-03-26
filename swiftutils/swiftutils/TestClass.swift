b3e3b0712bc8c80bad442d229bfa2c3bcd291c41
Mahesh Asabe <mahesh.asabe@in-it0544.globant.com>
2018-03-26 15:31:25 +0530

    Created swiftuitls initial xcode project

---
+++ swiftutils/swiftutils/TestClass.swift
@@ -0,0 +1,16 @@
+//
+//  TestClass.swift
+//  swiftutils
+//
+//  Created by Mahesh Asabe on 26/03/18.
+//  Copyright © 2018 Mahesh Asabe. All rights reserved.
+//
+
+import Foundation
+
+class TestClass: NSObject {
+    
+    class func getTestMessage() -> String {
+        return "Hello, testing, testing 123"
+    }
+}
